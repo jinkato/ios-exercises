c1d5dd2ebeb349ab1a84091487ba7561cb961cd4
Jin Kato <jin.kato@starcomww.com>
2015-10-23 13:06:21 -0400

    removed work from checkpoint 12

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,29 +12,7 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    
-    NSString *outputString = @"";
-    NSMutableString *tempI;
-    
-    if(number < otherNumber){
-        for (NSInteger i = number; i <= otherNumber; i++){
-            tempI = [NSMutableString stringWithFormat: @"%ld", (long)i];
-            outputString = [outputString stringByAppendingString:tempI];
-        }
-    }else if (number > otherNumber){
-        for (NSInteger i = otherNumber; i <= number; i++){
-            tempI = [NSMutableString stringWithFormat: @"%ld", (long)i];
-            outputString = [outputString stringByAppendingString:tempI];
-        }
-    }else if(number == otherNumber){
-        outputString = [NSMutableString stringWithFormat: @"%ld", (long)number];
-    }else{
-        NSLog(@"Something broke");
-    }
-    
-    
-    
-    return outputString;
+    return @"";
 }
 
 @end
--- BlocExercises/BlocExercises/AwesomeCounterTests.m
+++ BlocExercises/BlocExercises/AwesomeCounterTests.m
@@ -31,20 +31,13 @@
 }
 
 
-
 - (void)testThatStringWorksAscending {
-    
-    
     NSInteger lowNumber = -7;
     NSInteger highNumber = 13;
     NSString *expectedString = @"-7-6-5-4-3-2-1012345678910111213";
     NSString *actualString = [self.counter stringWithNumbersBetweenNumber:lowNumber andOtherNumber:highNumber];
-    
-    //output needs to be
     XCTAssertEqualObjects(expectedString, actualString, @"strings differed");
 }
-
-
 
 - (void)testThatStringWorksDescending {
     NSInteger lowNumber = 9;
@@ -53,8 +46,6 @@
     NSString *actualString = [self.counter stringWithNumbersBetweenNumber:lowNumber andOtherNumber:highNumber];
     XCTAssertEqualObjects(expectedString, actualString, @"strings differed");
 }
-
-
 
 - (void)testThatStringWorksEqual {
     NSInteger lowNumber = 94;
